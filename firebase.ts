dfe25701b616e9bca21a77aa6f5e3a25e724cc87
kaif0981 <saifalisaifali20036@gmail.com>
2026-01-31 16:30:24 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+// File: firebase.ts
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAfCRtUcaUypV-agKAzHUzMdyPzyEhTF5k",
   authDomain: "grant-meridian.firebaseapp.com",
@@ -17,6 +13,5 @@ const firebaseConfig = {
   measurementId: "G-3XR49CPJSS"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
